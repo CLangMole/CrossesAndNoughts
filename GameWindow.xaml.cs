9a65e92b238efacf8e99c76fea9c05eb672b7ce3
KrashDevelopment <116433214+KrashDevelopment@users.noreply.github.com>
2023-06-14 15:51:40 +0200

    Added visitor-symbols painter and symbols factory
    Коротше топ, використав ще два паттерна

--- GameWindow.xaml.cs
+++ GameWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CrossesAndNoughts.Symbols.Factories;
+using CrossesAndNoughts.Symbols;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +14,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace CrossesAndNoughts
+namespace CrossesAndNoughts;
+
+/// <summary>
+/// Interaction logic for GameWindow.xaml
+/// </summary>
+public partial class GameWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for GameWindow.xaml
-    /// </summary>
-    public partial class GameWindow : Window
+    public GameWindow()
     {
-        public GameWindow()
-        {
-            InitializeComponent();
-            Closed += (sender, e) => Application.Current.Shutdown();
-        }
+        InitializeComponent();
+        Closed += (sender, e) => Application.Current.Shutdown();
+
+        SymbolsFactory factory = new CrossesFactory(new Image() { Source = new BitmapImage() { UriSource = new Uri(@"C:\Users\probn\Fiverr\FiverrAssets\OIP.jpg") } });
+        Symbol symbol = factory.Create();
+        Field.Children.Add(new Image() { Source = new BitmapImage() { UriSource = new Uri(@"C:\Users\probn\Fiverr\FiverrAssets\OIP.jpg") } });
     }
 }
---
+++ Symbols/Factories/NoughtsFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CrossesAndNoughts.Symbols.Factories;
+
+abstract class NoughtsFactory : SymbolsFactory
+{
+    private Image _image;
+    public NoughtsFactory(Image image) : base(image) => _image = image;
+
+    public override Symbol Create()
+    {
+        return new Nought();
+    }
+}
