585b4a3358ca2cf77e53dade47f4459d638c6466
KrashDevelopment <116433214+KrashDevelopment@users.noreply.github.com>
2023-06-17 20:34:45 +0200

    Deleted commands

--- ViewModel/DBViewModel.cs
+++ ViewModel/DBViewModel.cs
@@ -9,20 +9,11 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using CrossesAndNoughts;
 using CrossesAndNoughts.View;
-using CrossesAndNoughts.ViewModel.Commands;
 
 namespace CrossesAndNoughts.ViewModel
 {
     public class DBViewModel : INotifyPropertyChanged
     {
-        public ICommand? GoNextCommand { get => _goNextCommand; protected set => _goNextCommand = (IDelegateCommand)value; }
-        private IDelegateCommand _goNextCommand;
-
-        public DBViewModel()
-        {
-            _goNextCommand = new LabelDelegateCommand(ExecuteGoNext, CanExecuteGoNext);
-        }
-
         private List<UserRecord> _records()
         {
             using (IRecord records = new UserRecordsProxy())
@@ -51,25 +42,5 @@ namespace CrossesAndNoughts.ViewModel
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
-
-        private void ExecuteGoNext(object parameters)
-        {
-            object[]? objects = parameters as object[];
-            foreach (UIElement nextControl in (UIElement[])objects[1])
-            {
-                if (nextControl == null) return;
-                nextControl.Visibility = Visibility.Visible;
-
-                Grid? grid = objects[0] as Grid;
-
-                if (grid?.Children.Count <= 0) return;
-                foreach (UIElement childrenControl in grid.Children)
-                {
-                    if (childrenControl == nextControl || childrenControl.Uid == "CollapsedAtStart") continue;
-                    childrenControl.Visibility = Visibility.Collapsed;
-                }
-            }
-        }
-        private bool CanExecuteGoNext(object parameter) => true;
     }
 }
--- obj/Debug/net6.0-windows/View/GameWindow.g.i.cs
+++ obj/Debug/net6.0-windows/View/GameWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace CrossesAndNoughts.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CrossesAndNoughts;V1.0.0.0;component/view/gamewindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CrossesAndNoughts;component/view/gamewindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\GameWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
